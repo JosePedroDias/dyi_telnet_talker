375fb4fb956f4a1a05a293370be784bc5d27cc3c
José Pedro Dias <jose.dias@arkadium.com>
2024-07-26 20:03:33 +0100

    trying to share info between threads v2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::{
-    io::{BufRead, BufReader, Write}, net::TcpListener, sync::{Arc, Mutex}, thread
+    io::{BufRead, BufReader, Write},
+    net::TcpListener,
+    sync::{Arc, RwLock},
+    thread,
 };
 
 /*
@@ -14,9 +17,9 @@ fn main() {
     for stream in listener.incoming() {
         println!("Connection established!");
 
-        let names = vec![];
-        let names = Mutex::new(names);
-        let names = Arc::new(names);
+        // shared info
+        let names = Arc::new(RwLock::new(vec![]));
+        let topic = Arc::new(RwLock::new(String::from("unset")));
 
         thread::spawn(move || {
             let names = Arc::new(names);
@@ -27,30 +30,53 @@ fn main() {
 
             let mut line_no = 0;
             let reader = BufReader::new(stream.try_clone().unwrap());
-            for line in reader.lines() {
-                match line {
-                    Ok(line) => {
+            for input in reader.lines() {
+                match input {
+                    Ok(input) => {
                         line_no += 1;
 
-                        println!("Received: {}", line);
+                        println!("Received: {}", input);
 
-                        if line == "exit" {
-                            stream.write(b"Bye!\r\n").unwrap();
-                            break;
+                        let mut reply;
+                        match input.as_str() {
+                            "exit" => {
+                                stream.write(b"Bye!\r\n").unwrap();
+                                break;
+                            }
+                            "list users" => {
+                                let names = names.read().unwrap();
+                                let joined_names = names.join(", ");
+                                reply = format!("Users online: {}", joined_names);
+                            }
+                            "get topic" => {
+                                let topic = topic.read().unwrap().clone();
+                                reply = format!("Current topic: {}", topic);
+                            }
+                            _ if line_no == 1 => {
+                                let my_name = input.clone();
+                                reply = format!("Hello, {}!", my_name);
+                                let mut names = names.write().unwrap();
+                                names.push(my_name.clone());
+                            }
+                            _ => {
+                                let tokens = input.split_whitespace().collect::<Vec<_>>();
+                                let first = tokens.get(0);
+                                let second = tokens.get(1);
+
+                                println!("tokens: {:?} {:?}", first, second);
+
+                                if first == Some(&"set") && second == Some(&"topic") {
+                                    let new_topic = tokens[2].to_string();
+                                    let mut topic = topic.write().unwrap();
+                                    *topic = new_topic.clone();
+                                    reply = format!("Topic was set to to {}", new_topic);
+                                } else {
+                                    reply = input.to_ascii_uppercase();
+                                }
+                            }
                         }
 
-                        let mut reply = line.to_ascii_uppercase() + "\r\n";
-                        if line_no == 1 {
-                            let my_name = line.clone();
-                            reply = format!("Hello, {}!\r\n", my_name);
-
-                            let mut names = names.lock().unwrap();
-                            names.push(my_name.clone());
-
-                            let joined_names = names.join(", ");
-                            reply = format!("{}Users here: {}\r\n", reply, joined_names);
-                        }
-
+                        reply += "\r\n";
                         stream.write(reply.as_bytes()).unwrap();
                     }
                     Err(e) => {
